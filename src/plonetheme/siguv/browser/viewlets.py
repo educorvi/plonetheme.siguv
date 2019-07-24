42cf1366690f23565cb77cc30a8fcad903500391
Stefan Arnold <stefan.arnold@operun.de>
2019-07-24 15:10:31 +0200

    added dropdowns and more styling

--- src/plonetheme/siguv/browser/viewlets.py
+++ src/plonetheme/siguv/browser/viewlets.py
@@ -9,6 +9,16 @@ class CollapsibleNav(GlobalSectionsViewlet):
     index = ViewPageTemplateFile('templates/collapsible_nav.pt')
 
     def return_navtree(self, path=None):
-        if path:
-            return self.navtree.get(path, [])
-        return self.navtree.get(self.navtree_path, [])
+        if not path:
+            path = self.navtree_path
+        return self.navtree.get(path, [])
+
+    def get_item_class(self, item, sub):
+        # We don't compute 'sub' from item as it expensive
+        # and it was already computed in the page template
+        css_class = u'nav-item'
+        if sub:
+            css_class += u' dropdown'
+        if item and 'id' in item and self.selected_portal_tab == item['id']:
+            css_class += u' active'
+        return css_class
